a07ad585e891679006dc03bb291e1a040ec8c1f8
Pablo Vozzi <105393481+pablito1153@users.noreply.github.com>
2022-08-29 22:14:01 -0300

    Add files via upload

---
+++ PROYECTO JAVASCRIPT/index.js
@@ -0,0 +1,67 @@
+let divisa = prompt("¿Qué moneda desea cambiar? (EURO, DOLAR, LIBRA, REAL)");
+
+/* while (divisa!== "dolar" || "euro" || "libra" || "real"){
+    alert("MONEDA NO ENCONTRADA");
+    divisa = prompt("¿Qué moneda desea cambiar? (EURO, DOLAR, LIBRA, REAL)");
+} */
+
+if (divisa == "dolar"){
+    alert("USTED VA A COMPRAR DOLARES");
+    let cantidad = prompt("INGRESAR LA CANTIDAD A CAMBIAR");
+    if (cantidad <= 100) {
+        alert("PRECIO FINAL $" + (cantidad * 310));
+    }else if (cantidad > 101 && cantidad < 500) {
+        alert("PRECIO FINAL $" + (cantidad * 290));
+    }else if (cantidad > 501) {
+        alert("PRECIO FINAL $" + (cantidad * 275));
+    }
+}
+    
+
+if (divisa == "euro"){
+    alert("USTED VA A COMPRAR EUROS");
+    let cantidad = prompt("INGRESAR LA CANTIDAD A CAMBIAR");
+    if (cantidad <= 100) {
+        alert("PRECIO FINAL $" + (cantidad * 330));
+    }else if (cantidad > 101 && cantidad < 500) {
+        alert("PRECIO FINAL $" + (cantidad * 315));
+    }else if (cantidad > 501) {
+        alert("PRECIO FINAL $" + (cantidad * 290));
+    }
+}
+
+if (divisa == "libra"){
+    alert("USTED VA A COMPRAR LIBRAS ESTERLINAS");
+    let cantidad = prompt("INGRESAR LA CANTIDAD A CAMBIAR");
+    if (cantidad <= 100) {
+        alert("PRECIO FINAL $" + (cantidad * 360));
+    }else if (cantidad > 101 && cantidad < 500) {
+        alert("PRECIO FINAL $" + (cantidad * 345));
+    }else if (cantidad > 501) {
+        alert("PRECIO FINAL $" + (cantidad * 330));
+    }
+}
+
+if (divisa == "real"){
+    alert("USTED VA A COMPRAR REALES");
+    let cantidad = prompt("INGRESAR LA CANTIDAD A CAMBIAR");
+    if (cantidad <= 100) {
+        alert("PRECIO FINAL $" + (cantidad * 60));
+    }else if (cantidad > 101 && cantidad < 500) {
+        alert("PRECIO FINAL $" + (cantidad * 58));
+    }else if (cantidad > 501) {
+        alert("PRECIO FINAL $" + (cantidad * 55));
+    }
+}
+
+let formaDePago = prompt("¿Cómo desea pagar? (CREDITO, DEBITO, TRANSFERENCIA)");
+let mensajeGracias = "GRACIAS POR SU COMPRA";
+
+if (formaDePago.toLowerCase = "credito"){
+    alert("Usted tendrá un 10% de recargo.");
+}else if(formaDePago.toLowerCase = "debito"){
+    alert("Usted tendrá un 5% de recargo.");
+}else if(formaDePago.toLowerCase = "transferencia"){
+    alert("Usted no tendrá recargo.");
+}
+alert(mensajeGracias);
